8681a5e35a29628d7ef60371d26df0379c3fe3bc
samllban <137834390+samllban@users.noreply.github.com>
2024-10-17 02:10:47 +0000

    printf confirmado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,5 +17,8 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+
+    printf("cartas\n");
+    
     return 0;
 }
