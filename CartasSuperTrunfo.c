ef6c49e97883decb2712a3c8a28c5fd293141aa9
samllban <137834390+samllban@users.noreply.github.com>
2024-11-26 02:20:29 +0000

    comparação dos super poderes

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,21 +3,15 @@
 
 typedef struct {
     char codigo[5];
-    char  nomeDaCidade[50];
     long int populacao;
     double area;
     double pib;
     int pontosTuristicos;
     double desidadePercapita;
-    double desidadePopulacional;
+    double desidadePopulacional; 
 }Cidade;
 
-
 void capturarDados(Cidade * cidade) {
-
-   
-    printf("Digite o nome da cidade: ");
-    fgets(cidade ->nomeDaCidade, 50, stdin);
 
     printf("Digite populacao: ");
     scanf("%ld", &cidade ->populacao);
@@ -37,8 +31,7 @@ void capturarDados(Cidade * cidade) {
 }
 
 void exibirCidades(const Cidade * cidade) {
-    
-    printf("Nome da cidade: %s\n", cidade -> nomeDaCidade);
+       
     printf("População: %ld\n", cidade ->populacao);
     printf("Area da cidade: %lf\n km", cidade ->area);
     printf("pib populacional: %lf\n", cidade ->pib);
@@ -51,12 +44,73 @@ void gerarCodigo(char estado, int numCidade, char * codigo) {
     snprintf(codigo, 5, "%c%02d\n", estado, numCidade );
 }
 
+double superPoderCalculador( const Cidade * cidade) {
+    return cidade -> populacao + cidade -> area + cidade -> pib + cidade -> pontosTuristicos
+}
+
+
+void compararCartas( const Cidade * cidade1, const Cidade * cidade2) {
+
+    //população 
+    if(cidade1 -> populacao > cidade2 -> populacao) {
+        printf("População da cidade: %s, vence com %lf numero de habitantes", cidade1 ->codigo, cidade1->populacao);
+    }
+    else if(cidade2 -> populacao > cidade1 -> populacao) {
+        printf("População da cidade: %s, vence com %lf numero de habitantes",cidade2 -> codigo, cidade2->populacao);
+    }
+    else {
+        printf("Valores populacionais iguais");
+    }
+
+    //area
+
+    if( cidade1 -> area > cidade2 -> area) {
+        printf("Area da cidade: %s, vence com %lf numeros de area", cidade1 -> codigo, cidade1 -> area);
+    } else if (cidade2 -> area > cidade2 -> area) {
+        printf("Area da cidade: %s, vence com %lf numeros de area", cidade2 -> codigo, cidade2 -> area);
+    } else {
+        printf("Valores de area iguais");
+    }
+
+    //pib 
+
+    if( cidade1 -> pib> cidade2 -> pib) {
+        printf("Pib da cidade: %s, vence com %lf ", cidade1 -> codigo, cidade1 -> pib);
+    } else if (cidade2 -> area > cidade2 -> area) {
+        printf("Area da cidade: %s, vence com %lf", cidade2 -> codigo, cidade2 -> area);
+    } else {
+        printf("Valores do pib iguais");
+    }
+
+    //pontos turisticos
+
+    if( cidade1 -> pontosTuristicos> cidade2 -> pontosTuristicos) {
+        printf("Ponto turiticos da cidade: %s, vence com %lf ", cidade1 -> codigo, cidade1 -> pontosTuriticos);
+    } else if (cidade2 -> area > cidade2 -> area) {
+        printf("Pontos turisticos da cidade : %s, vence com %lf", cidade2 -> codigo, cidade2 -> pontosTuristicos);
+    } else {
+        printf("Valores dos pontos turisticos são iguais");
+    }
+
+    double superPoder1 = superPoderCalculador(cidade1);
+    double superPoder2 = superPoderCalculador(cidade2);
+
+    if(superPoder1 > superPoder2)) {
+        printf("Cidade %s vence com super poder de %lf", cidade1 -> codigo, superPoder1);
+    } else if( superPoder2 > superPoder1) {
+        printf("Cidade %s vence com super de %lf", cidade2 ->codigo, superPoder2 );
+    } else {
+        printf("Valores dos super poderes iguais");
+    }
+        
+}
+
 int main(){
 
-    Cidade  cidades[8][4];
+    Cidade cidades[2][2];
 
-    for( char estado = 'A'; estado<= 'H'; estado++) {
-        for (int numCidade = 1; numCidade <= 4; numCidade++) {
+    for( char estado = 'A'; estado<= 'B'; estado++) {
+        for (int numCidade = 1; numCidade <= 2; numCidade++) {
             printf("Cadastro da cidade : %c%02d\n", estado, numCidade);
             gerarCodigo(estado, numCidade, cidades[estado - 'A'][numCidade - 1].codigo);
             capturarDados(&cidades[estado - 'A'][numCidade - 1]);
@@ -66,11 +120,10 @@ int main(){
 
     printf("Cidades cadastradas:\n");
     printf("----------------------------------\n");
-    for (char estado = 'A'; estado <= 'H'; estado++) {
-        for(int numCidade = 1; numCidade<=4; numCidade++ ) {
+    for (char estado = 'A'; estado <= 'B'; estado++) {
+        for(int numCidade = 1; numCidade<=2; numCidade++ ) {
             exibirCidades(&cidades[estado - 'A'][numCidade - 1]);
         }
     }
-    
     return 0;
 }
